5a6f719f9a07b29d0320d290296b22d0c32e07f5
yurii2007 <yurash862@gmail.com>
2024-09-22 20:08:12 +0300

    cli tutorial 1.1-1.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use anyhow::{ Result, Context };
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let file = File::open(&args.path).with_context(|| format!("could not read a file {:?}", &args.path))?;
+
+    // let file = match result {
+    //     Ok(file) => { file }
+    //     Err(error) => {
+    //         return Err(
+    //             format!("Error reading file {:?}: {}", &args.path.to_str(), error.to_string())
+    //         );
+    //     }
+    // };
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            if line.contains(&args.pattern) {
+                println!("{}", line);
+            }
+        }
+    }
+
+    Ok(())
 }
