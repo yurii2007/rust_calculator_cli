a0051cd56b2407c125ecd2c3fe344e8b8db7d04a
yurii2007 <yurash862@gmail.com>
2024-09-28 19:09:30 +0300

    ctrl + c handlers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use signal_hook::{ consts::SIGINT, iterator::Signals };
+use crossbeam_channel::{ bounded, tick, Receiver, select };
 use std::io::{ self, BufReader, Write, prelude::* };
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 use anyhow::{ Result, Context };
 
 use clap::Parser;
@@ -14,12 +18,41 @@ fn main() -> Result<()> {
     let pb = indicatif::ProgressBar::new(100);
     let args = Cli::parse();
 
+    // ctrlc::set_handler(|| {
+    //     println!("Ctrl + C pressed");
+    // }).expect("Error setting handler for Ctrl + C");
+
+    let mut signals = Signals::new([SIGINT])?;
+
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            println!("recevied signal {:?}", sig);
+        }
+    });
+
     let file = File::open(&args.path).with_context(||
         format!("could not read a file {:?}", &args.path)
     )?;
 
+    let ctrl_c_events = ctrl_channel()?;
+    let ticks = tick(Duration::from_secs(1));
+
+    loop {
+        select! {
+            recv(ticks) -> _ => {
+                println!("working");
+            }
+            recv(ctrl_c_events) -> _ => {
+                println!();
+                println!("Goodbye");
+                break;
+            }
+        }
+    }
+
     pb.inc(10);
 
+    thread::sleep(Duration::from_secs(2));
     let reader = BufReader::new(file);
 
     let stdout = io::stdout();
@@ -38,4 +71,13 @@ fn main() -> Result<()> {
 
     pb.finish();
     Ok(())
+}
+
+fn ctrl_channel() -> Result<Receiver<()>, ctrlc::Error> {
+    let (sender, receiver) = bounded::<()>(100);
+    let _ =ctrlc::set_handler(move || {
+        let _ = sender.send(());
+    });
+
+    Ok(receiver)
 }
